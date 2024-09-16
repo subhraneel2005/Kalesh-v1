12dbcfa9e77dc830a93d63d347c46cecf55eef43
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-09-16 14:00:22 +0530

    database setuo done

---
+++ server/prisma/migrations/20240916081159_newtablesadded/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `User` table. All the data in the column will be lost.
+  - You are about to alter the column `name` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(191)`.
+  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "email_verified_at" TIMESTAMP(3),
+ADD COLUMN     "email_verify_token" TEXT,
+ADD COLUMN     "password_reset_token" TEXT,
+ADD COLUMN     "token_send_at" TIMESTAMP(3),
+ALTER COLUMN "name" SET NOT NULL,
+ALTER COLUMN "name" SET DATA TYPE VARCHAR(191);
