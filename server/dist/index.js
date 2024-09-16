abf56b159ebdc8070e02af92ddc8e8726f30d3db
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-09-16 19:30:30 +0530

    EJS view engine setup done, combined four commands in one dev command ✅

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,8 +1,16 @@
 import express from "express";
 import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const PORT = process.env.PORT || 8000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+//settig ejs viewEngine
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
 app.get("/", (req, res) => {
-    return res.send("hat tere mai ke chodo");
+    return res.render("welcome");
 });
 app.listen(PORT, () => console.log(`Setavanga Server rumming on port = ${PORT}`));
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,21 @@
 import express, { Application,Response, Request } from "express";
 import "dotenv/config";
+import path from "path";
+import {fileURLToPath} from 'url';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const app:Application = express();
-
 const PORT = process.env.PORT || 8000;
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+//settig ejs viewEngine
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
+
 app.get("/", (req:Request, res:Response) => {
-    return res.send("hat tere mai ke chodo")
+    return res.render("welcome");
 })
 
 app.listen(PORT, () => console.log(`Setavanga Server rumming on port = ${PORT}`)
