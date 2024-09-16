1601d127afdd8c04730ecda672940e44042eea0d
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-09-16 17:05:41 +0530

    hom,epage UI done

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Hero from "@/components/base/Hero"
 
 export default function Home() {
   return (
    <div>
-    SetaVangha
+    <Hero/>
    </div>
   );
 }
---
+++ client/src/components/base/Hero.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className='min-h-screen w-full flex flex-col justify-center items-center'>
+        <div className='w-full h-full flex justify-center gap-10 px-4 items-center'>
+          <div className='flex justify-center items-center flex-col'>
+            <h1 className='md:text-[3.4rem] lg:text-[5rem] text-[1.8rem] text-center mb-12'>KALESH --v1</h1>
+            <p className='md:text-[1.5rem] text-[0.8rem] text-center text-purple-300 -mt-12'>Choose the better choice together</p>
+            <Link href={'/login'}>
+              <button className='px-6 py-3 mt-6 btn text-black'>Get Started</button>
+            </Link>
+          </div>
+          <Image src="/logo1.svg"  width={500} height={500} alt='bannerImage' className='-z-10 opacity-80 flex justify-center items-center'/>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
