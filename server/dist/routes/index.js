e061c834ec78eea584b110c590a62f9696dbe47e
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-09-17 19:36:06 +0530

    /veri-email route added to index.js main file

--- server/dist/routes/index.js
+++ server/dist/routes/index.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import AuthRoutes from "./authRoutes.js";
+import VerifyRoutes from "./verifyRoutes.js";
 const router = Router();
 router.use("/api/auth", AuthRoutes);
+router.use("/", VerifyRoutes);
 export default router;
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import AuthRoutes from "./authRoutes.js"
+import VerifyRoutes from "./verifyRoutes.js"
 
 const router = Router();
 
 router.use("/api/auth", AuthRoutes)
+router.use("/", VerifyRoutes)
 
 export default router;
