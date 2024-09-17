a17e644418c8d4d15911f962e9dd3c702460811a
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-09-18 04:02:36 +0530

    session coming ✅

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
 import Hero from "@/components/base/Hero"
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/options";
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await getServerSession(authOptions);
+
   return (
    <div>
+    {/* <p>{JSON.stringify(session)}</p> */}
     <Hero/>
    </div>
   );
