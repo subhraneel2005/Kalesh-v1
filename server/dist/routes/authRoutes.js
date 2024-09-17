df06dac3af72ad5dbd75d56c321139503f0af01d
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-09-17 14:25:51 +0530

    auth routes + auth validation done✅

--- server/dist/routes/authRoutes.js
+++ server/dist/routes/authRoutes.js
@@ -1,1 +1,15 @@
-export {};
+import { Router } from "express";
+import { registerSchema } from "../validation/authValidation.js";
+const router = Router();
+//Register route
+router.post("/", async (req, res) => {
+    try {
+        const body = req.body;
+        const payload = registerSchema.parse(body);
+        res.json(payload);
+    }
+    catch (error) {
+        return res.status(404).json(error);
+    }
+});
+export default router;
