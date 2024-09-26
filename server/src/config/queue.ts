f695826ddf2a2863b4a63e8b42812772003d29f1
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-09-26 22:08:22 +0530

    f

--- server/src/config/queue.ts
+++ server/src/config/queue.ts
@@ -4,6 +4,7 @@ export const redisConenction: ConnectionOptions = {
     password: process.env.REDIS_PASSWORD,
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
+    connectTimeout: 10000,
 };
 
 
